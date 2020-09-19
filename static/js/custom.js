b5a0ee7fd2a07c9806a5f40c238011af3ce4286f
Sam <heao8831@gmail.com>
2020-09-19 17:15:09 +0800

    second commit

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,18 +1,3 @@
-//$(document).ready(function() {
-//    $('form').on('submit', function(event) {
-//        $.ajax({
-//                data:{longurl: $('#longurl').val()},
-//                type:'POST',
-//                url: '/'
-//            })
-//            .done(function(data){
-//                contents = data.results;
-//                $('#shorturl').val = contents;
-//            });
-//        event.preventDefault();
-//    });
-//});
-//
 $(function() {
     $('#button').click(function() {
         $.ajax({
@@ -20,18 +5,9 @@ $(function() {
             data: $('form').serialize(),
             type: 'POST',
             success:function(response){
-                console.log(response);
-                console.log(response.short_url);
                  $('#shorturl').text(response.short_url)
             }
         })
-//        .done(function(data){
-//        console.log(data);
-//            console.log(data.short_url);
-//
-////            contents = data.results
-//            $('#shorturl').val = data.short_url
-//        });
         event.preventDefault();
     });
 });
